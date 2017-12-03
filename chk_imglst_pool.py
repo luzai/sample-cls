63d2970c6158aab03b79da140cef7e97adfb97a6
Xinglu Wang <907682447@qq.com>
2017-12-04 01:46:16 +0800

    try to fix imglst

--- chk_imglst_pool.py
+++ chk_imglst_pool.py
@@ -3,37 +3,39 @@ from lz import *
 import cv2
 
 
-def check_individual(filename, filepath):
-    filepath=osp.abspath(filepath)
+def check_individual(filepath):
+    filepath = osp.abspath(filepath)
     if not osp.exists(filepath):
         print('no exist', filepath)
         append_file(filepath.split('/')[-2], '/home/wangxinglu/fail.txt')
         # utils.rm(osp.dirname(filepath))
-        return
+        return False
     try:
         im = cv2.imread(filepath, cv2.IMREAD_COLOR)
-        stdout, stderr = shell('convert {} /dev/null'.format(filepath))  # -regard-warnings
+        stdout, stderr = shell('convert {} /dev/null '.format(filepath))  # -regard-warnings
         if stderr != b'':
             raise ValueError('corrupt! {} '.format(stderr))
     except Exception as inst:
         print(filepath, ' error ', inst)
         append_file(filepath, '/home/wangxinglu/corrupt.txt')
         # utils.rm(filepath)
-        return
-    # cv2.imwrite(filepath, im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        return False
+        # cv2.imwrite(filepath, im, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
     try:
         if im.shape[0] <= 16 or im.shape[1] <= 16:
             # utils.rm(filepath)
             append_file(filepath, '/home/wangxinglu/small.txt')
             print('rm small ', filepath)
-            return
+            return False
         assert im.shape[0] > 16 and im.shape[1] > 16
         assert (len(im.shape) == 3 and im.shape[-1] == 3), 'img ' + filepath + str(im.shape)
         assert im.shape[1] != 0 and im.shape[0] != 0, 'width ' + filepath + str(im.shape)
     except Exception as inst:
         print(filepath, ' error ', inst)
-        append_file(filepath,'/home/wangxinglu/corrupt.txt')
-        return
+        append_file(filepath, '/home/wangxinglu/corrupt.txt')
+        return False
+    return True
+
 
 # @chdir_to_root
 # def check_img(prefix='/home/wangxinglu/prj/few-shot/data/imagenet-raw'):
@@ -51,21 +53,25 @@ def check_individual(filename, filepath):
 @chdir_to_root
 def chk_img_lst(path):
     import multiprocessing as mp
-    pool = mp.Pool(32)
+    pool = mp.Pool(64)
     prefix = '/home/wangxinglu/prj/few-shot/data/imagenet-raw/'
-    cache=np.array(read_list(path))
-    # mypickle(cache,'cache.pkl')
-    # cache = unpickle('cache.pkl')
-    # print(cache, 'ok')
+    cache = np.array(read_list(path))
 
     for imgpath in cache[::-1, 0]:
-        pool.apply_async(check_individual, args=(imgpath.split('/')[-1], prefix + imgpath))
+        pool.apply_async(check_individual, args=(prefix + imgpath,))
     pool.close()
     pool.join()
 
+    # start = time.time()
+    # for imgpath in cache[:1000, 0]:
+    #     pool.apply_async(check_individual, args=(imgpath.split('/')[-1], prefix + imgpath))
+    # pool.close()
+    # pool.join()
+    # print('1000 imgs take', time.time() - start)
+
 
 if __name__ == '__main__':
-    # chk_img_lst(path='/home/wangxinglu/prj/few-shot/data/imglst/img10k.test.txt')
+    chk_img_lst(path='/home/wangxinglu/prj/few-shot/data/imglst/img10k.test.txt')
     chk_img_lst(path='/home/wangxinglu/prj/few-shot/data/imglst/img10k.train.txt')
     # check_img()
     # check_img(prefix='/mnt/nfs1703/kchen/imagenet-raw-trans-to-redis')
--- gen_imglst.py
+++ gen_imglst.py
@@ -3,14 +3,12 @@ For gen /home/wangxinglu/prj/few-shot/data/imglst/img1k.test.txt
 '''
 
 import lz as utils
-from metadata import *
 from lz import *
-import tensorflow as tf
+from chk_imglst_pool import check_individual
 
 # HOME = os.environ['HOME'] or '/home/wangxinglu'
 HOME = '/home/wangxinglu'
-cache_path= '/home/wangxinglu/prj/few-shot/src/nimgs.pkl'
-# cache_path= '/mnt/nfs1703/test/prj/cls-sample/nimgs.dbg.pkl'
+cache_path = '/home/wangxinglu/prj/few-shot/src/nimgs.pkl'
 num = 10000
 np.random.seed(64)
 
@@ -21,10 +19,11 @@ def cls_sample(num, prob=None):
 
     # leaves = {}
     # for node in tf.gfile.ListDirectory(prefix):
-    #     leaves[node] = len(tf.gfile.ListDirectory(prefix + '/' + node))
+    #     leaves[node] = (os.listdir(prefix + '/' + node))
     # mypickle(leaves,cache_path)
 
     name2nimg = unpickle('nimgs.pkl')
+    name2nimg = {name: len(fnames) for name, fnames in name2nimg.items()}
     name2nimg = {name: nimg for name, nimg in name2nimg.items() if nimg >= 10}
 
     nimgs = np.asarray(list(name2nimg.values()))
@@ -51,25 +50,38 @@ def cls_sample(num, prob=None):
     res = comb[res, :][:, 0]
     return res, res_nimgs
 
-def bsearch(nums,query):
-    nums=np.asarray(nums)
-    small=nums<query
-    res=np.arange(nums.shape[0])[small].max()
+
+def bsearch(nums, query):
+    nums = np.asarray(nums)
+    small = nums < query
+    res = np.arange(nums.shape[0])[small].max()
     return res
+
 
 @chdir_to_root
 def gen_imglst(names, prefix, train_file, test_file):
     os.chdir(prefix)
     imgs_train_l, imgs_test_l = [], []
+    name2imgs = unpickle('nimgs.pkl')
     for ind, cls in enumerate(names):
         if not osp.exists(cls): continue
-        imgs = tf.gfile.Glob(cls + '/*.JPEG')
+        imgs = name2imgs[cls]
+        imgs = [cls + '/' + img_ for img_ in imgs]
         if len(imgs) == 0:
-            utils.rm(cls, True)
+            utils.rm(cls)
             continue
         assert len(imgs) >= 10
         imgs = np.array(imgs)
+        checks = map(check_individual, imgs)
+
+        for check, img in zip(checks, imgs):
+            if not check:
+                imgs = np.setdiff1d(imgs, img)
+                # utils.rm(img)
+                print('corrupt ', img)
+
         imgs_test = np.random.choice(imgs, max(3, imgs.shape[0] * 1 // 10), replace=False)
+
         imgs_train = np.setdiff1d(imgs, imgs_test)
         # imgs_train = imgs[:imgs.shape[0] * 9 // 10]
         # imgs_test = imgs[imgs.shape[0] * 9 // 10:]
@@ -92,9 +104,6 @@ if __name__ == '__main__':
     # prob = [1.5, 0., 1.5]
     # train_file = HOME + '/prj/few-shot/data/imglst/img10k.train.no1k.txt'
     # test_file = HOME + '/prj/few-shot/data/imglst/img10k.test.no1k.txt'
-    # prefix = HOME + '/prj/few-shot/data/imagenet-raw'
-    # utils.write_list('/home/wangxinglu/prj/few-shot/data/imagenet10k.txt.chk', names, delimiter=' ', fmt='%s')
-    # gen_imglst(names, prefix, train_file, test_file)
 
     prob = [1.35, 0.7, 1.35]
     train_file = HOME + '/prj/few-shot/data/imglst/img10k.train.txt'
